3f425d78a9058f512f52c18982d1c99cafaac14c
ชาคริต ดิสกุล <izaac@Macintosh.lan>
2024-09-01 22:04:30 +0700

    add hamberger mavbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,7 +7,7 @@
           class="h-8" alt="Logo" />
         <span class="self-center text-2xl font-semibold whitespace-nowrap">ระบบจัดการพนักงาน</span>
       </div>
-      <button data-collapse-toggle="navbar-default" type="button"
+      <button @click="toggleMenu" type="button"
         class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
         aria-controls="navbar-default" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
@@ -16,24 +16,25 @@
             d="M1 1h15M1 7h15M1 13h15" />
         </svg>
       </button>
-      <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+      <div :class="menuOpen ? 'block' : 'hidden'" class="w-full md:block md:w-auto" id="navbar-default">
         <ul
           class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
           <li>
-            <router-link to="/" class="block py-2 px-3 rounded md:p-0" active-class="text-blue-700 md:text-blue-700"
+            <router-link @click="toggleMenu" to="/" class="block py-2 px-3 rounded md:p-0"
+              active-class="text-blue-700 md:text-blue-700"
               exact-active-class="text-white bg-blue-700 md:bg-transparent">
               รายชื่อพนักงาน
             </router-link>
           </li>
           <li>
-            <router-link to="/employee-form" class="block py-2 px-3 rounded md:p-0"
+            <router-link @click="toggleMenu" to="/employee-form" class="block py-2 px-3 rounded md:p-0"
               active-class="text-blue-700 md:text-blue-700"
               exact-active-class="text-white bg-blue-700 md:bg-transparent">
               เพิ่มพนักงาน
             </router-link>
           </li>
           <li>
-            <router-link to="/update-employee" class="block py-2 px-3 rounded md:p-0"
+            <router-link @click="toggleMenu" to="/update-employee" class="block py-2 px-3 rounded md:p-0"
               active-class="text-blue-700 md:text-blue-700"
               exact-active-class="text-white bg-blue-700 md:bg-transparent">
               แก้ไขพนักงาน
@@ -44,3 +45,18 @@
     </div>
   </nav>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.menuOpen = !this.menuOpen;
+    },
+  },
+};
+</script>
